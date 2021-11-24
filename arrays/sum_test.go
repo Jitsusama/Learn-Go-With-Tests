53b3dac4d3c1da034bfd986c2bcdf1900e878745
Joel Gerber <joel.gerber@molex.com>
2021-11-24 08:06:57 -0500

    handles summing tails for empty slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -26,10 +26,22 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails([]int{1, 2}, []int{0, 9})
-	want := []int{2, 9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
+	validateSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	}
+
+	t.Run("sums the tail of slices with 2 members", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		validateSums(t, got, want)
+	})
+
+	t.Run("sums the tail of a mixture of 3 and 0 member slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		validateSums(t, got, want)
+	})
 }
