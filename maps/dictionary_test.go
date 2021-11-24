05cc20e555f66facb5385fe7fac0ea2d410640ca
Joel Gerber <joel.gerber@molex.com>
2021-11-24 18:43:38 -0500

    complains on unknown words

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,9 +5,24 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
+	t.Run("finds a known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
 
-	assertStrings(t, got, "this is just a test")
+		assertStrings(t, got, "this is just a test")
+	})
+
+	t.Run("complains on unknown words", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
