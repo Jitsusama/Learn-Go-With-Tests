9192de980ccca3424867f8f42f96b7ff54e91dca
Joel Gerber <joel.gerber@molex.com>
2021-11-28 12:15:40 -0500

    add concurrency for speed (21ms/op)

--- concurrency/checker/checker.go
+++ concurrency/checker/checker.go
@@ -2,10 +2,24 @@ package checker
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsBuffer := make(chan result)
+
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultsBuffer <- result{u, wc(u)}
+		}(url)
 	}
+	for range urls {
+		r := <-resultsBuffer
+		results[r.string] = r.bool
+	}
+
 	return results
 }
