e1bcacb48743d50729336f1113ee52a0e83da57f
Joel Gerber <joel.gerber@molex.com>
2022-01-22 12:46:09 -0500

    incrementation is concurrent safe

--- sync/counter/counter_test.go
+++ sync/counter/counter_test.go
@@ -1,15 +1,40 @@
 package counter
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
-	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+	t.Run("increments the counter 3 times", func(t *testing.T) {
 		counter := Counter{}
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
-		if counter.Value() != 3 {
-			t.Errorf("got %d want %d", counter.Value(), 3)
-		}
+		assertCounter(t, &counter, 3)
 	})
+
+	t.Run("increments the counter concurrently", func(t *testing.T) {
+		var waiter sync.WaitGroup
+		count := 1000
+		counter := Counter{}
+
+		waiter.Add(count)
+		for i := 0; i < count; i++ {
+			go func() {
+				counter.Inc()
+				waiter.Done()
+			}()
+		}
+		waiter.Wait()
+
+		assertCounter(t, &counter, count)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d want %d", got.Value(), want)
+	}
 }
