09d193881884ad2a414c43fa3769930eff979e5e
Joel Gerber <joel.gerber@molex.com>
2022-01-23 14:49:40 -0500

    40 - 50 = XL - L

--- pbt/numerals/roman.go
+++ pbt/numerals/roman.go
@@ -8,7 +8,8 @@ type converter struct {
 }
 
 var conversions = []converter{
-	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
+	{50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"},
+	{5, "V"}, {4, "IV"}, {1, "I"},
 }
 
 func ConvertToRoman(arabic int) string {
--- pbt/numerals/roman_test.go
+++ pbt/numerals/roman_test.go
@@ -1,26 +1,27 @@
 package numerals
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestRomainNumerals(t *testing.T) {
-	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+func TestArabicToRomainNumeralConversions(t *testing.T) {
+	conversions := []struct {
+		Arabic int
+		Roman  string
 	}{
-		{"1 = I", 1, "I"}, {"2 = II", 2, "II"},
-		{"3 = III", 3, "III"}, {"4 = IV", 4, "IV"},
-		{"5 = V", 5, "V"}, {"6 = VI", 6, "VI"},
-		{"7 = VII", 7, "VII"}, {"8 = VIII", 8, "VIII"},
-		{"9 = IX", 9, "IX"}, {"10 = X", 10, "X"},
-		{"14 = XIV", 14, "XIV"}, {"18 = XVIII", 18, "XVIII"},
-		{"20 = XX", 20, "XX"}, {"39 = XXXIX", 39, "XXXIX"},
+		{1, "I"}, {2, "II"}, {3, "III"}, {4, "IV"},
+		{5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"},
+		{9, "IX"}, {10, "X"}, {14, "XIV"}, {18, "XVIII"},
+		{20, "XX"}, {39, "XXXIX"}, {40, "XL"}, {47, "XLVII"},
+		{49, "XLIX"}, {50, "L"},
 	}
 
-	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
-			if got := ConvertToRoman(test.Arabic); got != test.Want {
-				t.Errorf("got %q want %q", got, test.Want)
+	for _, c := range conversions {
+		d := fmt.Sprintf("%02d equals %v", c.Arabic, c.Roman)
+		t.Run(d, func(t *testing.T) {
+			if got := ConvertToRoman(c.Arabic); got != c.Roman {
+				t.Errorf("got %q want %q", got, c.Roman)
 			}
 		})
 	}
