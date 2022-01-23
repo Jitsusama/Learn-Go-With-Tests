32c16f97a87e95268ed31e993c2696feb5b61de7
Joel Gerber <joel.gerber@molex.com>
2022-01-23 14:58:35 -0500

    handles all arabic->roman numeral conversions

--- pbt/numerals/roman.go
+++ pbt/numerals/roman.go
@@ -3,21 +3,23 @@ package numerals
 import "strings"
 
 type converter struct {
-	Value  int
-	Symbol string
+	Arabic int
+	Roman  string
 }
 
 var conversions = []converter{
-	{50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"},
-	{5, "V"}, {4, "IV"}, {1, "I"},
+	{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+	{100, "C"}, {90, "LC"}, {50, "L"}, {40, "XL"},
+	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+	{1, "I"},
 }
 
 func ConvertToRoman(arabic int) string {
 	var roman strings.Builder
 	for _, conversion := range conversions {
-		for arabic >= conversion.Value {
-			roman.WriteString(conversion.Symbol)
-			arabic -= conversion.Value
+		for arabic >= conversion.Arabic {
+			roman.WriteString(conversion.Roman)
+			arabic -= conversion.Arabic
 		}
 	}
 	return roman.String()
--- pbt/numerals/roman_test.go
+++ pbt/numerals/roman_test.go
@@ -14,7 +14,9 @@ func TestArabicToRomainNumeralConversions(t *testing.T) {
 		{5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"},
 		{9, "IX"}, {10, "X"}, {14, "XIV"}, {18, "XVIII"},
 		{20, "XX"}, {39, "XXXIX"}, {40, "XL"}, {47, "XLVII"},
-		{49, "XLIX"}, {50, "L"},
+		{49, "XLIX"}, {50, "L"}, {90, "LC"}, {100, "C"},
+		{400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"},
+		{1984, "MCMLXXXIV"},
 	}
 
 	for _, c := range conversions {
