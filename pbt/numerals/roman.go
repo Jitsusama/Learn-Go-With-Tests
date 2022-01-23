8a0f5c26a41a32c35e183ef8c43aced3789ce67b
Joel Gerber <joel.gerber@molex.com>
2022-01-23 14:18:33 -0500

    10 - 39 = X - XXXIX

--- pbt/numerals/roman.go
+++ pbt/numerals/roman.go
@@ -2,19 +2,21 @@ package numerals
 
 import "strings"
 
+type converter struct {
+	Value  int
+	Symbol string
+}
+
+var conversions = []converter{
+	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var roman strings.Builder
-	for arabic > 0 {
-		switch {
-		case arabic > 4:
-			roman.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			roman.WriteString("IV")
-			arabic -= 4
-		default:
-			roman.WriteString("I")
-			arabic--
+	for _, conversion := range conversions {
+		for arabic >= conversion.Value {
+			roman.WriteString(conversion.Symbol)
+			arabic -= conversion.Value
 		}
 	}
 	return roman.String()
--- pbt/numerals/roman_test.go
+++ pbt/numerals/roman_test.go
@@ -8,9 +8,13 @@ func TestRomainNumerals(t *testing.T) {
 		Arabic      int
 		Want        string
 	}{
-		{"1 = I", 1, "I"}, {"2 = II", 2, "II"}, {"3 = III", 3, "III"},
-		{"4 = IV", 4, "IV"}, {"5 = V", 5, "V"}, {"6 = VI", 6, "VI"},
+		{"1 = I", 1, "I"}, {"2 = II", 2, "II"},
+		{"3 = III", 3, "III"}, {"4 = IV", 4, "IV"},
+		{"5 = V", 5, "V"}, {"6 = VI", 6, "VI"},
 		{"7 = VII", 7, "VII"}, {"8 = VIII", 8, "VIII"},
+		{"9 = IX", 9, "IX"}, {"10 = X", 10, "X"},
+		{"14 = XIV", 14, "XIV"}, {"18 = XVIII", 18, "XVIII"},
+		{"20 = XX", 20, "XX"}, {"39 = XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
