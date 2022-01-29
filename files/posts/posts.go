017c5de93f7cdfd54874fc24575bf8bff4668b0b
Joel Gerber <joel.gerber@molex.com>
2022-01-29 17:31:44 -0500

    grabs titles from posts

--- files/posts/posts.go
+++ files/posts/posts.go
@@ -1,20 +1,56 @@
 package posts
 
 import (
+	"io"
 	"io/fs"
 )
 
-type Post struct{}
+type Post struct {
+	Title string
+}
 
 func NewPostsFromFs(filesystem fs.FS) ([]Post, error) {
-	dir, err := fs.ReadDir(filesystem, ".")
+	filenames, err := getFilenames(filesystem)
 	if err != nil {
 		return nil, err
 	}
 
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, filename := range filenames {
+		post, err := getPost(filesystem, filename)
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getFilenames(filesystem fs.FS) ([]string, error) {
+	directory, err := fs.ReadDir(filesystem, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+	for _, file := range directory {
+		filenames = append(filenames, file.Name())
+	}
+	return filenames, nil
+}
+
+func getPost(filesystem fs.FS, filename string) (Post, error) {
+	file, err := filesystem.Open(filename)
+	if err != nil {
+		return Post{}, err
+	}
+	defer file.Close()
+
+	d, err := io.ReadAll(file)
+	if err != nil {
+		return Post{}, err
+	}
+
+	return Post{Title: string(d)[7:]}, nil
 }
--- files/posts/posts_test.go
+++ files/posts/posts_test.go
@@ -2,23 +2,29 @@ package posts_test
 
 import (
 	"jitsusama/lgwt/files/posts"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
 
 func TestNewPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts, err := posts.NewPostsFromFs(fs)
+	got, err := posts.NewPostsFromFs(fs)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts want %d posts", len(posts), len(fs))
+	if len(got) != len(fs) {
+		t.Errorf("got %d posts want %d posts", len(got), len(fs))
+	}
+
+	expected := posts.Post{Title: "Post 1"}
+	if !reflect.DeepEqual(got[0], expected) {
+		t.Errorf("got %+v want %+v", got[0], expected)
 	}
 }
