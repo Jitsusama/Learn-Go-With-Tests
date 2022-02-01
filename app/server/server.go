fcd694a8fadadbe4195d9121c2233a9a39491a5c
Joel Gerber <joel.gerber@molex.com>
2022-02-01 18:28:16 -0500

    complains on missing players

--- app/server/server.go
+++ app/server/server.go
@@ -16,5 +16,11 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.Store.GetScore(player))
+	score := p.Store.GetScore(player)
+
+	if score == 0 {
+		w.WriteHeader(404)
+	}
+
+	fmt.Fprint(w, score)
 }
--- app/server/server_test.go
+++ app/server/server_test.go
@@ -19,6 +19,7 @@ func TestPlayerRetrieval(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, 200)
 		assertBody(t, response.Body, "20")
 	})
 	t.Run("retrieve floyd's score", func(t *testing.T) {
@@ -27,7 +28,16 @@ func TestPlayerRetrieval(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, 200)
 		assertBody(t, response.Body, "10")
+	})
+	t.Run("complains on missing players", func(t *testing.T) {
+		request := getScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, 404)
 	})
 }
 
@@ -49,5 +59,12 @@ func assertBody(t testing.TB, body *bytes.Buffer, expected string) {
 	actual := body.String()
 	if actual != expected {
 		t.Errorf("got %q want %q", actual, expected)
+	}
+}
+
+func assertStatus(t testing.TB, actual int, expected int) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("got %d want %d", actual, expected)
 	}
 }
