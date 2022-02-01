453af6e21c51a001bd7ebf472a8fd03176f63300
Joel Gerber <joel.gerber@molex.com>
2022-02-01 18:16:47 -0500

    add stub player store to server bootstrapper

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -6,7 +6,13 @@ import (
 	"net/http"
 )
 
+type PlayerStoreInMemory struct{}
+
+func (s *PlayerStoreInMemory) GetScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &server.PlayerServer{}
+	server := &server.PlayerServer{Store: &PlayerStoreInMemory{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
