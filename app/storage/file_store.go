f91dddfdaf6d66438d0b872c7ee197bde57441e1
Joel Gerber <joel.gerber@molex.com>
2022-02-04 18:20:55 -0500

    read player score from JSON file

--- app/storage/file_store.go
+++ app/storage/file_store.go
@@ -11,10 +11,21 @@ type FilePlayerStore struct {
 }
 
 func (f *FilePlayerStore) GetLeague() []Player {
+	// reset position to beginning of file
 	f.database.Seek(0, 0)
+
 	var league []Player
 	if err := json.NewDecoder(f.database).Decode(&league); err != nil {
 		fmt.Printf("json parsing error: %v", err)
 	}
 	return league
+}
+
+func (f *FilePlayerStore) GetPlayerScore(name string) int {
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			return player.Wins
+		}
+	}
+	return 0
 }
--- app/storage/file_store_test.go
+++ app/storage/file_store_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestFileStorage(t *testing.T) {
-	t.Run("dummy test", func(t *testing.T) {
+	t.Run("reads league data from JSON file", func(t *testing.T) {
 		fileContents := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}
@@ -17,18 +17,37 @@ func TestFileStorage(t *testing.T) {
 		// read once
 		actual := fileStore.GetLeague()
 		expected := []Player{{"Cleo", 10}, {"Chris", 33}}
-		assertLeagueBody(t, actual, expected)
+		assertLeague(t, actual, expected)
 
 		// read once more
 		actual = fileStore.GetLeague()
 		expected = []Player{{"Cleo", 10}, {"Chris", 33}}
-		assertLeagueBody(t, actual, expected)
+		assertLeague(t, actual, expected)
+	})
+
+	t.Run("reads player score from JSON file", func(t *testing.T) {
+		fileContents := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}
+		]`)
+		fileStore := FilePlayerStore{fileContents}
+
+		actual := fileStore.GetPlayerScore("Chris")
+		expected := 33
+		assertScore(t, actual, expected)
 	})
 }
 
-func assertLeagueBody(t testing.TB, actual []Player, expected []Player) {
+func assertLeague(t testing.TB, actual []Player, expected []Player) {
 	t.Helper()
 	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v want %v", actual, expected)
+	}
+}
+
+func assertScore(t testing.TB, actual int, expected int) {
+	t.Helper()
+	if actual != expected {
 		t.Errorf("got %v want %v", actual, expected)
 	}
 }
